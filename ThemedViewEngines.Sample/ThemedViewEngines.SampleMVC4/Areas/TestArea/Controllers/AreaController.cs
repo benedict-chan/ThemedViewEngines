a3e3f17b0ff5072c682c40a77ce2fec71f6a8aa1
Benedict Chan <purple.enchanted@gmail.com>
2014-03-14 17:01:39 +1100

    Added Simple page for a sample site

---
+++ ThemedViewEngines.Sample/ThemedViewEngines.SampleMVC4/Areas/TestArea/Controllers/AreaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThemedViewEngines.SampleMVC4.Areas.TestArea.Controllers
+{
+    public class AreaController : Controller
+    {
+        //
+        // GET: /TestArea/Area/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
